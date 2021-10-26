973eb0cc72e5e4d31485bcc964fc4bd7f8a957af
paozanwa22 <fausanwadi@gmail.com>
2021-10-26 13:41:34 +0800

    menggunakan GetBuilder

--- lib/controllers/counter_controller.dart
+++ lib/controllers/counter_controller.dart
@@ -3,9 +3,10 @@
 import 'package:get/get.dart';
 
 class CounterController extends GetxController {
-  var count = 0.obs;
+  var count = 0;
 
   void increment(){
     count++;
+    update();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,22 @@ import './controllers/counter_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  // final controlC = Get.put(CounterController());
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(),
         body: Center(
-          child: GetX<CounterController>(
-            init: CounterController(),
+          child: GetBuilder<CounterController>(
+            init: CounterController(), //pengganti get.put
             builder: (controller) => Text(
-              'Angka ${controller.count.value}',
+              'Angka ${controller.count}',
               style: const TextStyle(
                 fontSize: 35,
               ),
@@ -24,6 +27,7 @@ class MyApp extends StatelessWidget {
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             Get.find<CounterController>().increment();
+            //controlC.increment();
           },
         ),
       ),
