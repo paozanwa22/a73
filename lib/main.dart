59b540efe1841d1e688b58270cae86a03e11af7f
paozanwa22 <fausanwadi@gmail.com>
2021-10-26 10:40:08 +0800

    menggunakan Reactive GetX

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,20 @@
-import './controllers/orang_controller.dart';
+import './controllers/counter_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final orangc = Get.put(OrangController());
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(),
         body: Center(
-          child: Obx(
-            () => Text(
-              'nama saya ${orangc.orang.value.nama}',
+          child: GetX<CounterController>(
+            init: CounterController(),
+            builder: (controller) => Text(
+              'Angka ${controller.count.value}',
               style: const TextStyle(
                 fontSize: 35,
               ),
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
-            orangc.changeUpperCase();
+            Get.find<CounterController>().increment();
           },
         ),
       ),
